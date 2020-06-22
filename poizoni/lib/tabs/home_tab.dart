c4e2b6365e7b20efab969c4b898a1ef8eff8b633
Ivan Knobel <cc18193@g.unicamp.br>
2020-06-22 11:09:42 -0300

    adicionar imagens pela câmera ou galeria

--- poizoni/lib/tabs/home_tab.dart
+++ poizoni/lib/tabs/home_tab.dart
@@ -1,46 +1,63 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+
+  String image;
+
   @override
   Widget build(BuildContext context) {
-
     return Stack(
-      children: <Widget>[
-        CustomScrollView(
-          slivers: <Widget>[
-            SliverToBoxAdapter(
-              child: Container(
-                alignment: Alignment.center,
-                child:
-                Padding(
-                  padding: EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 20.0),
-                  child: GestureDetector(
-                    child: SizedBox(
-                      height: 350.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 204, 204, 204),
-                          border: Border.all(
-                              color: Colors.black,
-                              width: 2.0
+        children: <Widget>[
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child:
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 20.0),
+                      child: GestureDetector(
+                        child: SizedBox(
+                          height: 350.0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 204, 204, 204),
+                              border: Border.all(
+                                  color: Colors.black,
+                                  width: 2.0
+                              ),
+                            ),
+                            child: Center(
+                              child: image==null ? Text("Clique para selecionar uma imagem") :
+                              Image.file(File(image)),
+                            ),
                           ),
                         ),
-                        child: Center(
-                          child: Text("Clique para selecionar uma imagem"),
-                        ),
+                        onTap: (){
+                          ImagePicker.pickImage(source: ImageSource.gallery).then((file){
+                            if (file == null) return;
+                            setState(() {
+                              image = file.path;
+                            });
+                          });
+                        },
                       ),
                     ),
-                    onTap: (){},
-                  ),
-                ),
-              )
-            ),
-            SliverToBoxAdapter(
-                child: Container(
-                  alignment: Alignment.center,
-                  child:
-                  Padding(
+                  )
+              ),
+              SliverToBoxAdapter(
+                  child: Container(
+                    alignment: Alignment.center,
                     padding: EdgeInsets.symmetric(horizontal: 80.0),
                     child: SizedBox(
                       height: 80.0,
@@ -54,20 +71,30 @@ class HomeTab extends StatelessWidget {
                           ),
                         ),
                         child: Center(
-                          child: Icon(Icons.photo_camera, size: 40.0,),
+                          child: IconButton
+                            (
+                            icon: Icon(Icons.photo_camera),
+                            iconSize: 40,
+                            onPressed: (){
+                              ImagePicker.pickImage(source: ImageSource.camera).then((file){
+                                if (file == null) return;
+                                setState(() {
+                                  image = file.path;
+                                });
+                              });
+                            },
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                )
-            ),
-            SliverToBoxAdapter(
-                child: Container(
-                  alignment: Alignment.center,
-                  child:
-                  Padding(
+                  )
+              ),
+              SliverToBoxAdapter(
+                  child: Container(
+                    alignment: Alignment.center,
                     padding: EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 10.0),
-                    child: RaisedButton(
+                    child:
+                    RaisedButton(
                       child: Container(
                         padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
                         child: Text(
@@ -81,11 +108,11 @@ class HomeTab extends StatelessWidget {
                       onPressed: (){},
                     ),
                   )
-                )
-            )
-          ],
-        )
-      ]
+              )
+            ],
+          )
+        ]
     );
   }
 }
+
