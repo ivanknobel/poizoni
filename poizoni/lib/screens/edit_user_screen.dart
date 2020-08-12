a2f25ac543e93b89fa902f3f19a09ea768ce153a
Ivan Knobel <cc18193@g.unicamp.br>
2020-08-12 11:10:31 -0300

    avancei a tela de editar usuário

--- poizoni/lib/screens/edit_user_screen.dart
+++ poizoni/lib/screens/edit_user_screen.dart
@@ -16,6 +16,7 @@ class _EditUserScreenState extends State<EditUserScreen> {
 
   bool _userEdited = false;
   Map _editedUserData = Map();
+  List _phones = List();
 
   @override
   Widget build(BuildContext context) {
@@ -27,77 +28,78 @@ class _EditUserScreenState extends State<EditUserScreen> {
           centerTitle: true,
 
         ),
-        body: ScopedModelDescendant<UserModel>(
-          builder: (context, child, model){
-            _editedUserData = model.userData;
-            return Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container( //TODO: botar isso num gestureDetector pra dar pra clicar e mudar/excluir a foto
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  model.userData["img"]
-                              ),
-                              fit: BoxFit.cover),
+        body: SingleChildScrollView(
+          child: ScopedModelDescendant<UserModel>(
+            builder: (context, child, model){
+              _editedUserData = model.userData;
+              _phones = List.from(model.userData["phones"]);
+              return Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container( //TODO: botar isso num gestureDetector pra dar pra clicar e mudar/excluir a foto
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    model.userData["img"]
+                                ),
+                                fit: BoxFit.cover),
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        width: 20,
-                      ),
-                      Text( //TODO: transformar isso em um textField pra mudar o nome
-                        model.userData["nome"],
-                        style: TextStyle(fontSize: 22),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        width: 20,
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.save),
-                        onPressed: () {
-                          //TODO: salvar as mudancas
-                        },
-                        alignment: Alignment.centerRight,
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Telefones de emergência:",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  SizedBox(height: 20,),
-                  ListView.separated(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: model.phones.length + 1,
-                    itemBuilder: (context, index) {
-                      if (index == model.phones.length)
+                        SizedBox(
+                          height: 100,
+                          width: 20,
+                        ),
+                        Text( //TODO: transformar isso em um textField pra mudar o nome
+                          model.userData["nome"],
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          width: 20,
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.save),
+                          onPressed: () {
+                            //TODO: salvar as mudancas
+                          },
+                          alignment: Alignment.centerRight,
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Telefones de emergência:",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(height: 20,),
+                    ListView.separated(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: model.phones.length + 1,
+                      itemBuilder: (context, index) {
+                        if (index == model.phones.length)
+                          return _newPhoneCard(context, index);
                         return _editPhoneCard(context, index);
-                      controllers[index]["name"] = TextEditingController();
-                      controllers[index]["number"] = TextEditingController();
-                      return _editPhoneCard(context, index);
-                    },
-                    separatorBuilder: (context, index){
-                      return Divider();
-                    },
-                  ),
-                ],
-              ),
-            );
-          },
+                      },
+                      separatorBuilder: (context, index){
+                        return Divider();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -105,7 +107,62 @@ class _EditUserScreenState extends State<EditUserScreen> {
 
   Widget _editPhoneCard(context, index){
     //TODO: mesma coisa que os telefones mas campos de texto para editar (vazios se for um novo) e botao para excluir
-    return Container();
+    return Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(labelText: "Etiqueta"),
+          onChanged: (text){
+            _userEdited = true;
+            setState(() {
+              _phones[index]["label"] = text;
+            });
+          },
+          maxLength: 30,
+          initialValue: _phones[index]["label"],
+        ),
+        Row(
+          children: [
+            SizedBox(
+              width: 100,
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "Número"),
+                onChanged: (text){
+                  _userEdited = true;
+                  setState(() {
+                    _phones[index]["phone"] = text;
+                  });
+                },
+                initialValue: _phones[index]["phone"],
+                keyboardType: TextInputType.phone,
+              ),
+            ),
+            IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.delete),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _newPhoneCard(context, index){
+    //TODO: mesma coisa que os telefones mas campos de texto para editar (vazios se for um novo) e botao para excluir
+    _phones.add({"label": "", "number":""}); //inclui um vazio pra ser editado
+    return Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(labelText: "Etiqueta"),
+          onChanged: (text){
+            _userEdited = true;
+            setState(() {
+              _phones[index]["label"] = text;
+            });
+          },
+          maxLength: 30,
+        )
+      ],
+    );
   }
 
   Future<bool> _requestPop(){
