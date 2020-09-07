920bf76348e78cf97ad11f507c0bbec5abc3049f
Murilo Sanches <murilo.de.paula2011@gmail.com>
2020-09-07 14:02:41 -0300

    localizcao atual pronta

--- poizoni/lib/tabs/hospital_tab.dart
+++ poizoni/lib/tabs/hospital_tab.dart
@@ -4,6 +4,11 @@ import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
+const double CAMERA_ZOOM = 16;
+const double CAMERA_TILT = 80;
+const double CAMERA_BEARING = 30;
+const LatLng SOURCE_LOCATION = LatLng(42.747932,-71.167889);
+const LatLng DEST_LOCATION = LatLng(37.335685,-122.0605916);
 
 class HospitalTab extends StatefulWidget {
   final Key _mapKey = UniqueKey();
@@ -34,12 +39,15 @@ class TheMap extends StatefulWidget
 class _MyAppState extends State<TheMap>
 {
   Completer<GoogleMapController> _controller = Completer();
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
   final Set<Marker> _markers = {};
-  LatLng _lastMapPosition = _center;
+  LatLng _lastMapPosition;
   MapType _currentMapType = MapType.normal;
 
-  Location location = new Location();
+  LocationData currentLocation;
+// a reference to the destination location
+  LocationData destinationLocation;
+// wrapper around the location API
+  Location location;
 
   static bool _serviceEnabled;
   static PermissionStatus _permissionGranted;
@@ -52,38 +60,77 @@ class _MyAppState extends State<TheMap>
     zoom: 11.0,
   );
 
-  Future<bool> _getLocation() async{
-    do {
-      _serviceEnabled = await location.serviceEnabled();
-      if (!_serviceEnabled) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled) {
+  @override
+  void initState() {
+    super.initState();
 
-        }
-      }
-    }
-    while(!_serviceEnabled);
-    do {
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (_permissionGranted != PermissionStatus.granted) {
+    location = new Location();
 
-        }
-      }
-    }
-    while(_permissionGranted != PermissionStatus.granted);
+    location.onLocationChanged.listen((LocationData cLoc) {
+      // cLoc contains the lat and long of the
+      // current user's position in real time,
+      // so we're holding on to it
+      currentLocation = cLoc;
+      updatePinOnMap();
 
-    final _locationData = await location.getLocation();
+    });
+    // set custom marker pins
+    //setSourceAndDestinationIcons();
+    // set the initial location
+    setInitialLocation();
   }
 
-  Future<void> _goToPosition1() async {
+  void updatePinOnMap() async {
+
+    // create a new CameraPosition instance
+    // every time the location changes, so the camera
+    // follows the pin as it moves with an animation
+    CameraPosition cPosition = CameraPosition(
+      zoom: CAMERA_ZOOM,
+      tilt: CAMERA_TILT,
+      bearing: CAMERA_BEARING,
+      target: LatLng(currentLocation.latitude,
+          currentLocation.longitude),
+    );
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_position1));
+    controller.animateCamera(CameraUpdate.newCameraPosition(cPosition));
+    // do this inside the setState() so Flutter gets notified
+    // that a widget update is due
+    setState(() {
+      // updated position
+      var pinPosition = LatLng(currentLocation.latitude,
+          currentLocation.longitude);
+
+      // the trick is to remove the marker (by id)
+      // and add it again at the updated location
+
+      }
+    );
+  }
+
+  void showPinsOnMap() {
+    // get a LatLng for the source location
+    // from the LocationData currentLocation object
+    var pinPosition = LatLng(currentLocation.latitude,
+        currentLocation.longitude);
+  }
+
+
+  void setInitialLocation() async {
+    // set the initial location by pulling the user's
+    // current location from the location's getLocation()
+    currentLocation = await location.getLocation();
+
+    // hard-coded destination for this example
+    destinationLocation = LocationData.fromMap({
+      "latitude": DEST_LOCATION.latitude,
+      "longitude": DEST_LOCATION.longitude
+    });
   }
 
   _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+    showPinsOnMap();
   }
 
   _onCameraMove(CameraPosition position) {
@@ -101,16 +148,22 @@ class _MyAppState extends State<TheMap>
   _onAddMarkerButtonPressed() {
     setState(() {
       _markers.add(
-        Marker(
-          markerId: MarkerId(_lastMapPosition.toString()),
-          position: _lastMapPosition,
-          infoWindow: InfoWindow(
-            title: 'This is a Title',
-            snippet: 'This is a snippet',
-          ),
-          icon: BitmapDescriptor.defaultMarker,
+      Marker(
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'This is a Title',
+          snippet: 'This is a snippet',
         ),
-      );
+        icon: BitmapDescriptor.defaultMarker,
+      ),
+    );
+  });
+  }
+
+  _setMarkernull() {
+    setState(() {
+      _markers.clear();
     });
   }
 
@@ -128,36 +181,49 @@ class _MyAppState extends State<TheMap>
   @override
   Widget build(BuildContext context)
   {
-    return return Scaffold(
+    CameraPosition initialCameraPosition = CameraPosition(
+        zoom: CAMERA_ZOOM,
+        tilt: CAMERA_TILT,
+        target: SOURCE_LOCATION
+    );
+    if (currentLocation != null) {
+      initialCameraPosition = CameraPosition(
+          target: LatLng(currentLocation.latitude,
+              currentLocation.longitude),
+          zoom: CAMERA_ZOOM,
+          tilt: CAMERA_TILT,
+      );
+    }
+    return Scaffold(
       //also this avoids it crashing/breaking when the keyboard is up
       resizeToAvoidBottomInset: false,
       body: Stack(
         children: <Widget>[
           GoogleMap(
+            myLocationEnabled: true,
+            compassEnabled: true,
+            tiltGesturesEnabled: false,
+
             onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 11.0,
-            ),
+            initialCameraPosition: initialCameraPosition,
             mapType: _currentMapType,
             markers: _markers,
             onCameraMove: _onCameraMove,
           ),
           Padding(
-            padding: EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(5.0),
             child: Align(
               alignment: Alignment.topRight,
               child: Column(
                 children: <Widget>[
+                  SizedBox(
+                    height: 60.0,
+                  ),
                   button(_onMapTypeButtonPressed, Icons.map),
                   SizedBox(
                     height: 16.0,
                   ),
-                  button(_onAddMarkerButtonPressed, Icons.add_location),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  button(_goToPosition1, Icons.location_searching),
+                  button((){}, Icons.phone),
                 ],
               ),
             ),
