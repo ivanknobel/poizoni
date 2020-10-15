296686047fae4d4720eb74f8fd5ee8e2b02ff6d4
Murilo Sanches <murilo.de.paula2011@gmail.com>
2020-10-15 12:56:31 -0300

    a

--- poizoni/lib/tabs/home_tab.dart
+++ poizoni/lib/tabs/home_tab.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class HomeTab extends StatefulWidget {
   @override
@@ -11,108 +12,162 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-
   String image;
+  bool _loading;
+  File _image;
+  List _outputs;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model_unquant.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    Tflite.close();
+    super.dispose();
+  }
+
+  pickImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = image;
+    });
+    classifyImage(image);
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _loading = false;
+      _outputs = output;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-        children: <Widget>[
-          CustomScrollView(
-            slivers: <Widget>[
-              SliverToBoxAdapter(
-                  child: Container(
-                    alignment: Alignment.center,
-                    child:
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
-                      child: GestureDetector(
-                        child: SizedBox(
+    return _loading
+        ? Container(
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(),
+          )
+        : Stack(children: <Widget>[
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                    child: Container(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+                    child: GestureDetector(
+                      child: SizedBox(
                         height: 350.0,
                         child: Container(
                           decoration: BoxDecoration(
                             color: Color.fromARGB(255, 204, 204, 204),
-                            border: Border.all(
-                                color: Colors.black,
-                                width: 2.0
-                            ),
+                            border: Border.all(color: Colors.black, width: 2.0),
                           ),
                           child: Center(
-                            child: image==null ? Text("Clique para selecionar uma imagem") :
-                            Image.file(File(image)),
+                            child: _image == null
+                                ? Text("Clique para selecionar uma imagem")
+                                : Image.file(_image),
                           ),
                         ),
                       ),
-                        onTap: (){
-                          ImagePicker.pickImage(source: ImageSource.gallery).then((file){
-                            if (file == null) return;
-                            setState(() {
-                              image = file.path;
-                            });
-                          });
-                        },
+                      onTap: () {
+                        pickImage();
+                      },
+                    ),
+                  ),
+                )),
+                _outputs != null
+                    ? SliverToBoxAdapter(
+                        child: Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.symmetric(horizontal: 80.0),
+                            child: SizedBox(
+                                height: 80.0,
+                                child: Text(
+                                  "${_outputs[0]["label"]}",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                    background: Paint()..color = Colors.white,
+                                  ),
+                                ))))
+                    : SliverToBoxAdapter(
+                  child: Container(),
+                ),
+                SliverToBoxAdapter(
+                    child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(horizontal: 80.0),
+                  child: SizedBox(
+                    height: 20.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 204, 204, 204),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2.0,
+                        ),
+                      ),
+                      child: Center(
+                        child: IconButton(
+                          icon: Icon(Icons.photo_camera),
+                          iconSize: 40,
+                          onPressed: () {
+                            pickImage();
+                          },
+                        ),
                       ),
                     ),
-                  )
-              ),
-              SliverToBoxAdapter(
-                  child: Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.symmetric(horizontal: 80.0),
-                    child: SizedBox(
-                      height: 80.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color.fromARGB(255, 204, 204, 204),
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2.0,
-                          ),
-                        ),
-                        child: Center(
-                          child: IconButton
-                            (
-                            icon: Icon(Icons.photo_camera),
-                            iconSize: 40,
-                            onPressed: (){
-                              ImagePicker.pickImage(source: ImageSource.camera).then((file){
-                                if (file == null) return;
-                                setState(() {
-                                  image = file.path;
-                                });
-                              });
-                            },
-                          ),
-                        ),
+                  ),
+                )),
+                SliverToBoxAdapter(
+                    child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 10.0),
+                  child: RaisedButton(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 15.0),
+                      child: Text(
+                        "Pesquisar animal",
+                        style: TextStyle(fontSize: 18.0),
                       ),
                     ),
-                  )
-              ),
-              SliverToBoxAdapter(
-                  child: Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 10.0),
-                    child:
-                    RaisedButton(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
-                        child: Text(
-                          "Pesquisar animal",
-                          style: TextStyle(
-                              fontSize: 18.0
-                          ),
-                        ),
-                      ),
-                      color: Theme.of(context).primaryColor,
-                      onPressed: (){},
-                    ),
-                  )
-              )
-            ],
-          )
-        ]
-    );
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      pickImage();
+                    },
+                  ),
+                ))
+              ],
+            )
+          ]);
   }
 }
-
